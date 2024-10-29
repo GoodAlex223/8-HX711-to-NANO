f148a9e209ed68c011e40cb9c84bd0569e9cd981
goodalex223 <alexminak32@gmail.com>
2024-10-29 19:58:48 +0200

    Added calibration of load cells

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
 # Current project url -- https://wokwi.com/projects/412452836213719041
 
 ## TODO
-- [ ] Check if user can send nan from serial monitor while calibrating
-- 
+
+- [x] Check "c" command
+    - [x] Check save to memory
+    - [x] Check not save to memory
+- [x] Check "m" command
+- [x] Check "d" command
+
+- [x] Check "t" command
+- [x] Check "c" command
+- [x] Check if user can send nan from serial monitor while calibrating. User cant send nan
+
+## BUGS
+
+- [x] If 0 passed to calibration then nan is saved. FIXED: do not allow enter 0 as known mass
+- [ ] CRITICAL(I cant repeat it more then 3 times while tests): Simulation stops while changing weight on load cells
+- [ ] CRITICAL(I cant repeat it more then 1 times while tests): Simulation stop while re-taring
 
 ## MAYBE TODO
-// Если происходит калибровка - то происходит калибровка
-// If user send inapropriate data: попросить ввести еще раз или использовать дефолтное значение
-// Когда были введено число, спросить сохранить его в память или нет
-    // Если сохранить, то перепроветь ячейку памяти, если там не нан, то сказать что в этой ячейке памяти есть данные и спросить перезаписать их. Написать, если нет, то новое число не будет сохранено
-    // Если перезаписать, то перезаписать
-    // Если нет, то написать что данные не созхранены
-// Если использовать данные из памяти
-// Провеить каждую ячейку на наличие нан(не число)
-    // Если нан, то сказать что для такой-то лоадселл значение нан и спросить запустить ли калибровку этого датчика или использовать дефолтное значение
-    // Если запустить калибровку, то запустить калибровку(см. иф выше)
-    // Если нет, то этому датчику присвоить дефолтный скейл
-    // Если не нан, то этому датчику присвоить дефолтный скейл
-// Если использовать дефолтные значения, то всем датчикам присвоить дефолтный скейл
+
+// If calibration occurs - calibration occurs
+// If user send inapropriate data: ask to enter again or use default value
+// When a number has been entered, ask to save it to memory or not
+    // If save, then re-compute the memory location, if there is no nan there, then say that there is data in this memory location and ask to overwrite it. Write, if not, the new number will not be saved
+    // If overwrite, then overwrite
+    // If not, write that the data is not stored
+// If using data from memory
+// Check each cell for nan(not a number)
+    // If nan, then say that for such and such lowadsell the value is nan and ask whether to start calibration of this sensor or use the default value.
+    // If run calibration, then run calibration(see if above)
+    // If not, assign a default skale to this sensor
+    // If not nan, assign a default skail to this sensor
+// If default values are used, assign a default skail to all sensors
+
+- [ ] Ideas from https://wokwi.com/projects/407626388355231745
 
 ## Project sources
 
--- sketch.ino
+++ sketch.ino
@@ -1,95 +1,280 @@
-#include <HX711-multi.h>
+#include "HX711-multi.h"
 #include <EEPROM.h>
+// Include math library for isnan() to recalibrate load cells with no calibration values in EEPROM
+// And if user send not a number from serial monitor
+#include <math.h>
 
 // Pins
-#define CLK A0 // Common clock pin for all HX711 units
-#define DOUT1 A1
-#define DOUT2 A2
-#define DOUT3 A3
-#define DOUT4 A4
+#define CLK 2 // Common clock pin for all HX711 units
+#define DOUT1 3
+#define DOUT2 4
+#define DOUT3 5
+#define DOUT4 6
+#define DOUT5 7
+#define DOUT6 8
+#define DOUT7 9
+#define DOUT8 10
+// #define DOUT5 A5
+
+#define TARE_TIMEOUT_SECONDS 4
 
 // Array of data pins
-byte DOUTS[5] = {DOUT1, DOUT2, DOUT3, DOUT4};
+byte DOUTS[8] = {DOUT1, DOUT2, DOUT3, DOUT4, DOUT5, DOUT6, DOUT7, DOUT8};
 
-// Calibration factors for each load cell
-float calibrationFactors[4] = {1.0, 1.0, 1.0, 1.0};
-const int calibrationEEPROMAddress = 0;  // Starting EEPROM address for calibration factors
+// Calibration values for each load cell
+// float calibrationValues[8] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+float calibrationValues[8] = {0.42, 0.42, 0.42, 0.42, 0.42, 0.42, 0.42, 0.42};
+const int calibrationEEPROMAddress = 0;  // Starting EEPROM address for calibration values
 
 // Number of load cells
 #define CHANNEL_COUNT sizeof(DOUTS) / sizeof(byte)
 
-// Variables
+// Initialize HX711 units
+// define clock and data pin, channel, and gain value
+// channel selection is made by passing the appropriate gain: 128 or 64 for channel A, 32 for channel B
+// count: the number of channels
+// dout: an array of pin numbers, of length 'count', one entry per channel
+// pd_sck: clock pin
+// gain: 
+//  case 128:	GAIN = 1;	// channel A, gain value 128
+//  case 64: GAIN = 3; // channel A, gain value 64
+//  case 32: GAIN = 2; // channel B, gain value 32
+// HX711MULTI(int count, byte *dout, byte pd_sck, byte gain = 128);
 HX711MULTI scales(CHANNEL_COUNT, DOUTS, CLK);
 long results[CHANNEL_COUNT];
-bool tareComplete = false;
+int i = 0;
 
 void setup() {
   Serial.begin(57600);
-  Serial.println("Starting...");
+  Serial.println("Starting setuping...");
+  // Serial.flush();
+  // pinMode(11, OUTPUT);  // For some reason, if you add this pin, the console starts to display the text
 
-  tare();  // Tare each load cell before calibration
-  Serial.println("Tare complete.");
+  tare();
 
-  calibrate();  // Start calibration process
+  // Ask if you want to calibrate, use values from memory, or use default values
+  Serial.println("***");
+  Serial.println("To calibrate load cells, send 'c' from serial monitor");
+  Serial.println("To use load cells calibration values from EEPROM adress, send 'm' from serial monitor");
+  Serial.println("To use default load cells calibration values, send 'd' from serial monitor");
+
+  bool _resume = false;
+  while (_resume == false) {
+    if (Serial.available() > 0) {
+      char command = Serial.read();
+      switch (command) {
+        case 'c':
+          calibrate(); // Start calibration process
+          _resume = true;
+          break;
+        case 'm':
+          loadCalibrationFromEEPROM();
+          _resume = true;
+          break;
+        case 'd':
+          _resume = true;
+      }
+    }
+  }
+  Serial.println("Finishing setuping...");
+  Serial.println("***");
+  Serial.println("To re-calibrate, send 'c' from serial monitor.");
+  Serial.println("To set the tare offsets, send 't' from serial monitor.");
 }
 
 void loop() {
+  Serial.print("Number of read:");
+  Serial.print(i);
+  Serial.println();
+  sendRawData(); //this is for sending raw data, for where everything else is done in processing
+  i++;
+  delay(1000);
+
   if (Serial.available() > 0) {
     char command = Serial.read();
-    if (command == 'r') calibrate();
-    if (command == 't') tare();
+    if (command == 't') tare(); //tare
+    else if (command == 'c') calibrate(); //calibrate
+    // else if (command == 'e') changeSavedCalFactor(); //edit calibration value manually
   }
 }
 
+void sendRawData() {
+  // Serial.println("Before");
+  scales.read(results);
+  // Serial.println("After");
+  for (int i=0; i < scales.get_count(); ++i) {
+    // Serial.println("Before");
+    Serial.print(i+1);
+    Serial.print(":");
+    Serial.print(results[i] / calibrationValues[i]);
+    Serial.print("  ");
+    // Serial.println("After");
+    // Serial.print( (i!=scales.get_count()-1)?"\t":"\n");
+  }
+  // Serial.println("After 2");
+  Serial.println();
+}
+
 void tare() {
+  Serial.println("***");
+  Serial.println("Place the load cells an a level stable surface.");
+  Serial.println("Remove any load applied to the load cells.");
+  Serial.println("To set the tare offset, send 't' from serial monitor.");
+  Serial.println("To skip setting tare offset, send 's' from serial monitor.");
+  Serial.println("Current data of load cells:");
+  sendRawData();
+
+  bool _resume = false;
+  char command;
+  while(_resume == false){
+    while(!Serial.available());
+    command = Serial.read();
+    if (command == 't') {
+      _resume = true;
+    } 
+    else if (command == 's'){
+      Serial.println("Skipping setting tare offset.");
+      return;
+    }
+  }
+
   Serial.println("Taring...");
-  while (!scales.tare(20, 10000));  // Tare all load cells
+  bool tareSuccessful = false;
+  // Tare all load cells
+  unsigned long tareStartTime = millis();
+  while (!tareSuccessful && millis() < (tareStartTime + TARE_TIMEOUT_SECONDS*1000)) {
+    // Serial.println("Before");
+    tareSuccessful = scales.tare(20, 10000);  //reject 'tare' if still ringing
+    // Serial.println("After");
+  }
+  if (!tareSuccessful){
+    Serial.println("Timeout, check wiring and pin designations");
+    while (1);
+  }
+  Serial.println("Current data of load cells:");
+  sendRawData();
   Serial.println("Tare complete.");
-  tareComplete = true;
 }
 
 // Calibrate each load cell individually
 void calibrate() {
-  Serial.println("Starting calibration...");
+  // loadCalibrationFromEEPROM();
+  // for (int i = 0; i < CHANNEL_COUNT; i++) {
+  //   if (isnan(calibrationValues[i])){
+  //     Serial.println("ABOBA");
+  //   }
+  // }
+  Serial.println("***");
+  Serial.println("Start calibration:");
+
+  tare();
 
   for (int i = 0; i < CHANNEL_COUNT; i++) {
+    Serial.print("Calibrating ");
+    Serial.print(i + 1);
+    Serial.print(" load cell. To skip calibrating of this load cell and use previous calibrating value, send 's'. To continue, send any");
+
+    char command;
+    while (!Serial.available());
+    command = Serial.read();
+    if (command == 's') continue;
+
     Serial.print("Place known weight on load cell ");
     Serial.print(i + 1);
-    Serial.println(" and enter the weight (e.g., 100.0): ");
+    Serial.println(" and enter the weight (in grams)(e.g., 100.0): ");
 
     // Wait for known weight input
     float knownMass = 0;
-    while (Serial.available() == 0) {}
-    knownMass = Serial.parseFloat();
-    Serial.print("Known weight: ");
-    Serial.println(knownMass);
+    boolean _resume = false;
+    while (_resume == false) {
+      // scales.read(results);
+      while(!Serial.available());
+      
+      knownMass = Serial.parseFloat();
+      if (knownMass != 0){
+        Serial.print("Known weight: ");
+        Serial.println(knownMass);
+
+        Serial.println("Continue(y/n):");
+        while (!Serial.available());
+        command = Serial.read();
+        if (command == 'y'){
+          _resume = true;
+        } else {
+          Serial.print("Place known weight on load cell ");
+          Serial.print(i + 1);
+          Serial.println(" and enter the weight (in grams)(e.g., 100.0): ");
+        }
+          // TODO: Maybe add isnan check here
+      }
+    }
 
     // Measure raw data with known weight
     scales.read(results);
     long rawValue = results[i];
-    calibrationFactors[i] = rawValue / knownMass;  // Calculate calibration factor
+    calibrationValues[i] = rawValue / knownMass;  // Calculate calibration value
+    // // calibrationValues cant be 0
+    if (isnan(calibrationValues[i]) || calibrationValues[i] == 0){
+      calibrationValues[i] = 1;
+    }
 
-    Serial.print("Calibration factor for load cell ");
+    Serial.print("Calibration value for load cell ");
     Serial.print(i + 1);
     Serial.print(": ");
-    Serial.println(calibrationFactors[i]);
+    Serial.println(calibrationValues[i]);
     
-    // Save calibration factor to EEPROM
     int address = calibrationEEPROMAddress + (i * sizeof(float));
-    EEPROM.put(address, calibrationFactors[i]);
-    delay(500);
+    Serial.print("Save this value to EEPROM adress ");
+    Serial.print(address);
+    Serial.println("? y/n");
+
+    _resume = false;
+    while (_resume == false) {
+      while (!Serial.available());
+      command = Serial.read();
+      if (command == 'y') {
+// #if defined(ESP8266)|| defined(ESP32)
+//         EEPROM.begin(512);
+// #endif
+        // Save calibration value to EEPROM
+        EEPROM.put(address, calibrationValues[i]);
+// #if defined(ESP8266)|| defined(ESP32)
+//         EEPROM.commit();
+// #endif
+        EEPROM.get(address, calibrationValues[i]);
+        Serial.print("Value ");
+        Serial.print(calibrationValues[i]);
+        Serial.print(" saved to EEPROM address: ");
+        Serial.println(address);
+        _resume = true;
+      }
+      else if (command == 'n') {
+        Serial.println("Value not saved to EEPROM");
+        _resume = true;
+      }
+    }
   }
 
-  Serial.println("Calibration complete.");
+  Serial.println("End calibration");
+  Serial.println("***");
+  delay(500);
 }
 
 void loadCalibrationFromEEPROM() {
   for (int i = 0; i < CHANNEL_COUNT; i++) {
     int address = calibrationEEPROMAddress + (i * sizeof(float));
-    EEPROM.get(address, calibrationFactors[i]);
-    Serial.print("Loaded calibration factor for load cell ");
+    EEPROM.get(address, calibrationValues[i]);
+    Serial.print("Loaded calibration value for load cell ");
     Serial.print(i + 1);
     Serial.print(": ");
-    Serial.println(calibrationFactors[i]);
+    Serial.println(calibrationValues[i]);
+    // if (isnan(calibrationValues[i])){
+    //   Serial.println("Calibrate value is nan. Reasigning");
+    //   calibrationValues[i] = 1;
+    //   Serial.println("New calibration value for load cell ");
+    //   Serial.print(i + 1);
+    //   Serial.print(": ");
+    //   Serial.println(calibrationValues[i]);
+    // }
   }
 }
